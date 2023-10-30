9839d9ab8fbee073cea639f8235c326898f074ba
Elliot Iddon <elliot@land.tech>
2023-10-30 14:34:55 +0000

    Initial implementation

--- coffee.py
+++ coffee.py
@@ -1,23 +1,43 @@
 """
-Design a loyalty program for a coffee shop. Customers collect one stamp per coffee; when they reach six stamps, they have one free coffee. The application exposes the following public operations.
+Design a loyalty program for a coffee shop. Customers collect one stamp
+per coffee; when they reach six stamps, they have one free coffee.
+The application exposes the following public operations.
 
 The Rules:
  - I've provided some types but please change them!
  - There are some provided rules for this challenge which was originally in Java. I've adapted them to Python but not tested them. We can flex if it is too hard, even that will be an interesting outcome.
-   - Logic Classes (i.e. not dataclasses) can have a maximum of 2 fields and 4 methods.
-   - Method bodies have a maximum of 3 lines of code, no tricky code to get around this.
+   - Logic Classes (i.e. not dataclasses) can have a maximum of 2 fields
+     and 4 methods.
+   - Method bodies have a maximum of 3 lines of code, no tricky code
+     to get around this.
 """
 
+from typing import Dict
+
+class NotEnoughStampsError(Exception):
+  pass
+
+class CustomerAlreadyExistsError(Exception):
+  pass
+
 class CoffeeShop:
-  def create_customer(id: str):
-    pass
+  customer_stamps: Dict[str, int]
 
-  def create_stamp_by_customer(id: str):
-    pass
+  def __init__(self):
+    self.customer_stamps = {}
 
-  def list_stamps_and_coffees_by_customer(id: str) -> (int, int):
-    pass
+  def create_customer(self, id: str) -> None:
+    if id in self.customer_stamps:
+      raise CustomerAlreadyExistsError()
+    self.customer_stamps[id] = 0
 
-  def redeem_free_coffee_for_customer(id: str):
-    pass
+  def create_stamp_by_customer(self, id: str) -> None:
+    self.customer_stamps[id] += 1
 
+  def count_stamps_by_customer(self, id: str) -> int:
+    return self.customer_stamps[id]
+
+  def redeem_free_coffee_for_customer(self, id: str) -> None:
+    if self.customer_stamps[id] < 6:
+      raise NotEnoughStampsError()
+    self.customer_stamps[id] -= 6
--- test_coffee.py
+++ test_coffee.py
@@ -1,5 +1,51 @@
 import pytest
 import coffee
 
-def test_pytest():
-  pass
+def test_create_customer__creates_customer_with_no_stamps():
+  coffee_shop = coffee.CoffeeShop()
+  coffee_shop.create_customer("foo")
+  assert coffee_shop.count_stamps_by_customer("foo") == 0
+  
+def test_create_customer__creates_customer_with_one_stamp():
+  coffee_shop = coffee.CoffeeShop()
+  coffee_shop.create_customer("foo")
+  coffee_shop.create_stamp_by_customer("foo")
+  assert coffee_shop.count_stamps_by_customer("foo") == 1
+
+def test_count_stamps_by_customer__non_existent_customer():
+  coffee_shop = coffee.CoffeeShop()
+  with pytest.raises(KeyError):
+    coffee_shop.count_stamps_by_customer("foo")
+
+def test_create_stamp_by_customer__non_existent_customer():
+  coffee_shop = coffee.CoffeeShop()
+  with pytest.raises(KeyError):
+    coffee_shop.create_stamp_by_customer("foo")
+
+def test_redeem_free_coffee_for_customer():
+  coffee_shop = coffee.CoffeeShop()
+  coffee_shop.create_customer("foo")
+  for _ in range(6):
+    coffee_shop.create_stamp_by_customer("foo")
+  coffee_shop.redeem_free_coffee_for_customer("foo")
+  assert coffee_shop.count_stamps_by_customer("foo") == 0
+
+def test_redeem_free_coffee_for_customer():
+  coffee_shop = coffee.CoffeeShop()
+  coffee_shop.create_customer("foo")
+  for _ in range(15):
+    coffee_shop.create_stamp_by_customer("foo")
+  coffee_shop.redeem_free_coffee_for_customer("foo")
+  assert coffee_shop.count_stamps_by_customer("foo") == 9
+
+def test_redeem_free_coffee_not_enough_stamps():
+  coffee_shop = coffee.CoffeeShop()
+  coffee_shop.create_customer("foo")
+  with pytest.raises(coffee.NotEnoughStampsError):
+    coffee_shop.redeem_free_coffee_for_customer("foo")
+
+def test_create_customer__existing_raises_exception():
+  coffee_shop = coffee.CoffeeShop()
+  coffee_shop.create_customer("foo")
+  with pytest.raises(coffee.CustomerAlreadyExistsError):
+    coffee_shop.create_customer("foo")
